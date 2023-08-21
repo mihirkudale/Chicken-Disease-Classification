1d507e7745f87194bac1635e617611cb5ab464e0
Mihir Kudale <90020715+mihirkudale@users.noreply.github.com>
2023-08-21 15:30:48 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Chicken-Disease-Classification--Project"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "Chicken-Disease-Classification"
+AUTHOR_USER_NAME = "Mihir"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "mihirkudale1197@gmail.com"
 
 
 setuptools.setup(
